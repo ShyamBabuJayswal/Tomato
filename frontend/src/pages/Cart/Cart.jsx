5ef1c54ddb929443b2a1efd76c43a96443353577
NIhal <jaisnj541@gmail.com>
2025-01-01 13:53:56 +0530

    Add PlaceOrder component with delivery form and cart total section

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react'
 import "./Cart.css"
 import {StoreContext} from "../../context/StoreContext"
+import {useNavigate} from "react-router-dom"
 
 const Cart = () => {
     
-    const {cartItem,food_list,removeFromCart,getTotalCartAmount}=useContext(StoreContext);
+    const {cartItem,food_list,removeFromCart,getTotalCartAmount}= useContext(StoreContext);
+   
+    const navigate =useNavigate();
 
 
     return(
@@ -67,7 +70,7 @@ const Cart = () => {
         </div>
          
               </div>
-             <button>PROCEED TO CHECKOUT</button>
+             <button onClick={()=>navigate('/order') }>PROCEED TO CHECKOUT</button>
 
             </div>
             <div className='cart-promocode'>
