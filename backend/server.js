9a446a506bcd3d99ae21b68a9bd72f5e69fd10ae
NIhal <jaisnj541@gmail.com>
2025-01-03 11:19:47 +0530

    Add food upload functionality with image storage using multer

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
 
 //app config
@@ -14,13 +15,17 @@ const port = 4000
 app.use(express.json())
 
 app.use(cors())
+//db connection
+connectDB()
+
+//api endpoints
+app.use("/api/food",foodRouter)
 
 app.get('/',(req,res)=>{
     res.send("API WORKING")
 })
 
-//db connection
-connectDB()
+
 
 
 app.listen(port,()=>{
