ffbceec98296b212392d10011f456e375a6b36aa
NIhal <shyambabu_jayswal@yahoo.com>
2025-02-02 21:17:29 +0530

    Created cart router

---
+++ backend/routes/cartRoute.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+import { addToCart,removeFromCart,getCart } from "../controllers/CartController";
+
+const cartRouter = express.Router();
+
+cartRouter.post("/add",addToCart)
+
+cartRouter.post("/remove",removeFromCart)
+
+cartRouter.post("/get",getCart)
+
+
+export default cartRouter;
+
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -5,7 +5,7 @@ import {useNavigate} from "react-router-dom"
 
 const Cart = () => {
     
-    const {cartItem,food_list,removeFromCart,getTotalCartAmount}= useContext(StoreContext);
+    const {cartItem,food_list,removeFromCart,getTotalCartAmount,url}= useContext(StoreContext);
    
     const navigate =useNavigate();
 
@@ -31,7 +31,7 @@ const Cart = () => {
                 return(
         <div key={index}>
         <div  className='cart-items-title cart-items-item'>
-         <img src={item.image} alt=''/>
+         <img src={url+"/images/"+item.image} alt=''/>
              <p>{item.name}</p>
              <p>${item.price}</p>
              <p>{cartItem[item._id]}</p>
