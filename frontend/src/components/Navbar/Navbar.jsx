a96f122ed8549ed04926beb0ad65f4dbf3d91a68
NIhal <shyambabu_jayswal@yahoo.com>
2025-02-02 16:00:52 +0530

    Implement logout functionality with token removal and redirection

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,24 @@
 import React, { useContext, useState } from 'react';
 import './Navbar.css'
 import {assets} from '../../assets/assets'
-import {Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
 import { StoreContext } from '../../context/StoreContext';
 
 const Navbar = ({setShowLogin})=>{
 
     const [menu,setMenu]  = useState("home");
+    const navigate=useNavigate()
     
     const { getTotalCartAmount,token,setToken} = useContext(StoreContext);
+
+    const logout =()=>{
+        localStorage.removeItem("token")
+        setToken("");
+        navigate("/")
+
+        
+
+    }
 
 
 
@@ -42,7 +52,7 @@ const Navbar = ({setShowLogin})=>{
          <p>Orders</p>
              </li>
              <hr/>
-             <li>
+             <li onClick={logout}>
                 <img src={assets.logout_icon}/>
                 <p>logout</p>
              </li>
