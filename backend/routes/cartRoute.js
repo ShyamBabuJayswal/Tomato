12f243a52af9dfee6fbf04ba8421afc2a6498350
shyambabujayswal <jaisnj541@gmail.com>
2025-02-03 20:44:47 +0530

    Fix token verification logic in authMiddleware

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,14 +1,15 @@
 import express from "express";
 
 import { addToCart,removeFromCart,getCart } from "../controllers/CartController";
+import authMiddleware from "../middleware/auth.js";
 
 const cartRouter = express.Router();
 
-cartRouter.post("/add",addToCart)
+cartRouter.post("/add",authMiddleware,addToCart)
 
-cartRouter.post("/remove",removeFromCart)
+cartRouter.post("/remove",authMiddleware,removeFromCart)
 
-cartRouter.post("/get",getCart)
+cartRouter.post("/get",authMiddleware,getCart)
 
 
 export default cartRouter;
