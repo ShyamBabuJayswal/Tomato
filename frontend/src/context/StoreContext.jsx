3a81593d4e3fc18c5eef3a785f2edce925697861
shyambabujayswal <jaisnj541@gmail.com>
2025-02-04 17:28:13 +0530

    Implement API calls to load and remove cart items.

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -21,7 +21,7 @@ const StoreContextProvider = (props) => {
 
 
 
-    const addToCart = (itemId) => {
+    const addToCart = async (itemId) => {
        if(!cartItem[itemId]){
          setCartItem((prev)=>({...prev,[itemId]:1}))
        }
@@ -30,12 +30,22 @@ const StoreContextProvider = (props) => {
          ...prev,[itemId]:prev[itemId]+1
         }))
        } 
+       if(token){
+         await axios.post(url+"/api/cart/add",{itemId},{headers:{
+            token
+         }})
+       }
     }
-    const removeFromCart = (itemId) => {
+    const removeFromCart =async (itemId) => {
         setCartItem((prev)=>(
             {
         ...prev,[itemId]:prev[itemId]-1
         }))
+        if(token){
+         await axios.post(url+"/api/cart/remove",{itemId},{headers:{
+            token
+         }})   
+        }
     }
    const getTotalCartAmount  = () => {
     let totalAmount = 0;
@@ -54,12 +64,22 @@ const response = await axios.get(url+"/api/food/list")
 setFoodList(response.data.data);
    }
 
+  const loadCartData = async(token) => { 
+     const response =await axios.post(url+"/api/cart/get",{},
+       { headers:{token
+
+        }}
+      )
+      setCartItem(response.data.cartData)
+  } 
+
    useEffect(()=>{
       
      async function loadData(){
         await fetchFoodList();
         if(localStorage.getItem("token")){
             setToken(localStorage.getItem("token"));
+            await loadCartData(localStorage.getItem("token"));
         }
      }
     loadData();
