149435bcaa10cf1bcfbd2180a3d3ee52bc8e0517
shyambabujayswal <jaisnj541@gmail.com>
2025-02-05 20:41:02 +0530

    Fetch user orders based on userId with error handling.

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -2,12 +2,13 @@ import express from "express"
 
 import authMiddleware from "../middleware/auth.js";
 
-import { placeOrder, verifyOrder } from "../controllers/OrderController.js";
+import { placeOrder, userOrder, verifyOrder } from "../controllers/OrderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place",authMiddleware,placeOrder);
 orderRouter.post("/verify",authMiddleware,verifyOrder);
+orderRouter.post("/userorders",authMiddleware,userOrder);
 
 
 
