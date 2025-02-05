2e14a63a8868535a761f441836b16221a7929a1d
shyambabujayswal <jaisnj541@gmail.com>
2025-02-05 16:44:49 +0530

    Ensure userId is included in order data and add authentication check

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,32 +1,126 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import "./PlaceOrder.css"
 import { useNavigate } from 'react-router-dom'
 import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
 
 const PlaceOrder = () => {
    const navigate = useNavigate();
-   const {getTotalCartAmount} = useContext(StoreContext);
+   const { getTotalCartAmount, token, food_list, cartItem, url, userId } = useContext(StoreContext); 
 
+
+   const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: ""
+  });
+  
+
+   const onChangeHander = (event) => {
+         const name = event.target.name;
+         const value = event.target.value;
+         setData(data => ({
+          ...data,[name]:value
+         }))
+   }
+
+   const placeOrder = async (event) => {
+    event.preventDefault();
+
+    if (!userId) {
+        alert("User not authenticated. Please log in.");
+        return;
+    }
+
+    let orderItems = [];
+    food_list.forEach((item) => {
+        if (cartItem[item._id] > 0) {
+            let itemInfo = { ...item, quantity: cartItem[item._id] };
+            orderItems.push(itemInfo);
+        }
+    });
+
+    let orderData = {
+        userId,  // Ensure userId is sent
+        address: data,
+        items: orderItems,
+        amount: getTotalCartAmount() + 2,
+    };
+
+    try {
+        let response = await axios.post(url + "/api/order/place", orderData, {
+            headers: { token },
+        });
+
+        if (response.data.success) {
+            window.location.replace(response.data.session_url);
+        } else {
+            alert("Error placing order");
+        }
+    } catch (error) {
+        console.error("Order placement error:", error);
+        alert("Failed to place order. Try again.");
+    }
+};
+      
+   
 
   return (
-    <form className='place-order'>
+    <form onSubmit={placeOrder} className='place-order'>
  <div className='place-order-left'>
     <p className='title'>Delivery Information</p>
     <div className='multi-fields'>
-      <input type='text' placeholder='First Name'/>
-      <input type='text' placeholder='Last Name'/>
+      <input 
+      name='firstName'
+       onChange={onChangeHander}
+        type='text'
+         placeholder='First Name'
+          value={data.firstName}
+         />
+      <input 
+      name='lastName'
+      type='text'
+       placeholder='Last Name'
+        onChange={onChangeHander}
+        value={data.lastName}
+       />
     </div>
-    <input type='text' placeholder='Email address'/>
-    <input type='text' placeholder='Street'/>
+    <input 
+    name='email'
+    onChange={onChangeHander}
+    value={data.email}
+
+    type='text' placeholder='Email address'/>
+    <input
+     name='street'
+     value={data.street}
+     onChange={onChangeHander}
+     type='text' 
+     placeholder='Street'
+
+     />
     <div className='multi-fields'>
-      <input type='text' placeholder='City'/>
-      <input type='text' placeholder='State'/>
+      <input required name='city' onChange={onChangeHander} value={data.city} type='text' placeholder='City'/>
+       <input required name='state' onChange={onChangeHander} value={data.state}
+      type='text' placeholder='State'/>
     </div>
-    <div className='multi-fields'>
-      <input type='text' placeholder='Zip Code'/>
-      <input type='text' placeholder='Country'/>
+    <div required className='multi-fields'>
+      <input required name='zipcode' onChange={onChangeHander} value={data.zipcode}
+       type='text' placeholder='Zip Code'/>
+
+      <input required 
+       name='country' onChange={onChangeHander} value={data.country}
+      type='text' placeholder='Country'/>
     </div>
-    <input type='text' placeholder='Phone'/>
+    <input required
+     name='phone' onChange={onChangeHander} value={data.phone}
+    type='text' placeholder='Phone'/>
 
 
     </div>
@@ -53,7 +147,7 @@ const PlaceOrder = () => {
         </div>
          
               </div>
-             <button>PROCEED TO PAYMENT</button>
+             <button type='submit'>PROCEED TO PAYMENT</button>
 
             </div>
 
